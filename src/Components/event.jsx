412452a0b2d5a7aa16128ca6e62b21ecc3be4525
unknown <shubham@insaid.co>
2023-09-01 12:45:43 +0530

    event page chnages

--- src/Components/event.jsx
+++ src/Components/event.jsx
@@ -90,41 +90,7 @@ export default function Event() {
     return (
       <>
         <Myaccountnav />
-        <Box display="flex" sx={{ float: "right", mr: 2 }}>
-          <Typography
-            sx={{
-              mt: 10,
-              mr: 1,
-              fontWeight: "bold",
-              fontSize: "1rem",
-              cursor: "pointer",
-              "&:hover": {
-                color: "#0047b3",
-                cursor: "pointer",
-              },
-              textShadow:
-                "0 1px 0 #ccc,0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);",
 
-              mb: 5,
-            }}
-            onClick={sandlink}
-          >
-            {" "}
-            Go to Calendar
-          </Typography>
-          <EastIcon
-            sx={{
-              fontWeight: "bold",
-              mt: 10,
-              textShadow:
-                "0 1px 0 #ccc,0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);",
-              "&:hover": {
-                color: "#0047b3",
-                cursor: "pointer",
-              },
-            }}
-          />
-        </Box>
 
         <Box
           sx={{
